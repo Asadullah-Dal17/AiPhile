3a385d3903fcebdae008512fe37f8e4aadcc74b5
asadullah-dal-17 <asadullah.dal17@gmail.com>
2022-01-14 09:28:06 +0500

    adding image reader function

--- AiPhile.py
+++ AiPhile.py
@@ -1,4 +1,4 @@
-'''
+"""
 -------------------------------------------
 -    Author: Asadullah Dal                -
 -    =============================        -
@@ -8,47 +8,79 @@
 -    ============================         -
 -    Link: https://youtube.com/c/aiphile  -
 -------------------------------------------
-'''
-import cv2 as cv 
+"""
+import cv2 as cv
 import numpy as np
-
+import os
 
 # colors
-BLACK = (0,0,0)
-WHITE = (255,255,255)
-BLUE = (255,0,0)
-RED = (0,0,255)
-CYAN = (255,255,0)
-YELLOW =(0,255,255)
-MAGENTA = (255,0,255)
-GREEN = (0,255,0)
-PURPLE = (128,0,128)
-ORANGE = (0,165,255)
-PINK = (147,20,255)    
-INDIGO=[75,0,130]   
-VIOLET=[238,130,238]   
-GRAY=[127,127,127]  
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (255, 0, 0)
+RED = (0, 0, 255)
+CYAN = (255, 255, 0)
+YELLOW = (0, 255, 255)
+MAGENTA = (255, 0, 255)
+GREEN = (0, 255, 0)
+PURPLE = (128, 0, 128)
+ORANGE = (0, 165, 255)
+PINK = (147, 20, 255)
+INDIGO = [75, 0, 130]
+VIOLET = [238, 130, 238]
+GRAY = [127, 127, 127]
 
-# Draw text with background 
-def textBG(img, text, position, fonts ,scaling=1, color=(0,255,0), thickness=1, padding=3):
+
+def readImagesDir(path, resize_flag=None):
+    files = os.listdir(path)
+    list_path = []
+    img_list = []
+    for file in files:
+        img_path = os.path.join(path, file)
+        list_path.append(img_path)
+        img = cv.imread(img_path)
+        if resize_flag:
+            img = cv.resize(img, resize_flag, interpolation=cv.INTER_CUBIC)
+        img_list.append(img)
+        # cv.imshow('test', img)
+        # cv.waitKey(0)
+    return img_list
+
+
+# Draw text with background
+def textBG(
+    img, text, position, fonts, scaling=1, color=(0, 255, 0), thickness=1, padding=3
+):
     img_h, img_w = img.shape[:2]
     x, y = position
-    (w, h ), p= cv.getTextSize(text, fonts, scaling, thickness)
+    (w, h), p = cv.getTextSize(text, fonts, scaling, thickness)
     # print(w, h)
-    cv.rectangle(img, (x-p, y+p), (x+w+p, y-h-p), (255, 0,234), -1)
-    
-    cv.putText(img, text, position,fonts, scaling,  color, thickness)
+    cv.rectangle(img, (x - p, y + p), (x + w + p, y - h - p), (255, 0, 234), -1)
 
-# draw text with background having outlines 
-def textBGoutline(img, text, position=(40,30), fonts=cv.FONT_HERSHEY_SIMPLEX ,scaling=1, text_color=(0,255,0), thickness=1, bg_color=(BLACK)):
+    cv.putText(img, text, position, fonts, scaling, color, thickness)
+
+
+# draw text with background having outlines
+def textBGoutline(
+    img,
+    text,
+    position=(40, 30),
+    fonts=cv.FONT_HERSHEY_SIMPLEX,
+    scaling=1,
+    text_color=(0, 255, 0),
+    thickness=1,
+    bg_color=(BLACK),
+):
     img_h, img_w = img.shape[:2]
     x, y = position
-    (w, h ), p= cv.getTextSize(text, fonts, scaling, thickness)
+    (w, h), p = cv.getTextSize(text, fonts, scaling, thickness)
     # print(w, h)
-    cv.rectangle(img, (x-p, y+p), (x+w+p, y-h-p), bg_color, -1)
-    cv.rectangle(img, (x-p, y+p), (x+w+p, y-h-p), text_color,thickness, cv.LINE_AA)
-    
-    cv.putText(img, text, position,fonts, scaling,  text_color, thickness, cv.LINE_AA    )
+    cv.rectangle(img, (x - p, y + p), (x + w + p, y - h - p), bg_color, -1)
+    cv.rectangle(
+        img, (x - p, y + p), (x + w + p, y - h - p), text_color, thickness, cv.LINE_AA
+    )
+
+    cv.putText(img, text, position, fonts, scaling, text_color, thickness, cv.LINE_AA)
+
 
 # draw shape with transparency, from 0. to 1
 def fillPolyTrans(img, points, color, opacity, line_thickness=2):
@@ -62,10 +94,9 @@ def fillPolyTrans(img, points, color, opacity, line_thickness=2):
     """
     list_to_np_array = np.array(points, dtype=np.int32)
     overlay = img.copy()  # coping the image
-    cv.fillPoly(overlay,[list_to_np_array], color )
+    cv.fillPoly(overlay, [list_to_np_array], color)
     new_img = cv.addWeighted(overlay, opacity, img, 1 - opacity, 0)
     # print(points_list)
     img = new_img
-    cv.polylines(img, [list_to_np_array], True, color,line_thickness, cv.LINE_AA)
+    cv.polylines(img, [list_to_np_array], True, color, line_thickness, cv.LINE_AA)
     return img
-
