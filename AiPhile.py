467579ec343ec84cc005613b1eb2a1d486e32a51
Asadullah Dal <asadullah.dal17@gmail.com>
2023-07-29 13:09:26 +0500

    generate random color and use them

--- AiPhile.py
+++ AiPhile.py
@@ -9,9 +9,48 @@
 -    Link: https://youtube.com/c/aiphile  -
 -------------------------------------------
 """
+import os
+import random
+
 import cv2 as cv
 import numpy as np
-import os
+
+
+def get_random_rgb_color():
+    """Returns a random RGB color."""
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return r, g, b
+
+
+def get_contrast(color1, color2):
+    """Returns the contrast between two RGB colors."""
+    r1, g1, b1 = color1
+    r2, g2, b2 = color2
+    return (255 * (r1 - r2) ** 2 + 255 * (g1 - g2) ** 2 + 255 * (b1 - b2) ** 2) ** 0.3
+
+
+def get_list_of_random_rgb_colors_with_decent_contrast(number_of_colors):
+    """Returns a list of random RGB colors with decent contrast."""
+    list_of_colors = [(255, 255, 0)]
+    for _ in range(number_of_colors):
+        r, g, b = get_random_rgb_color()
+        while get_contrast((r, g, b), list_of_colors[-1]) < 100:
+            r, g, b = get_random_rgb_color()
+
+        # color_list.append([(r, g, b), (br, bg, bb)])
+        list_of_colors.append((r, g, b))
+    return list_of_colors
+
+
+def convert_flat_list_to_list_of_two_values(flat_list):
+    """Converts a flat list into a list of two two-values lists."""
+    list_of_two_values = []
+    for i in range(0, len(flat_list), 2):
+        sublist = flat_list[i : i + 2]
+        list_of_two_values.append(sublist)
+    return list_of_two_values
 
 
 def read_images_from_dir(path, resize_flag=None):
@@ -117,7 +156,13 @@ def fill_poly_trans(image, points, color, opacity):
     return image
 
 
-def trans_circle(image, org, radi, color, opacity):
+def trans_circle(
+    image,
+    org,
+    radi,
+    color,
+    opacity,
+):
     overlay = image.copy()  # coping the image
     cv.circle(overlay, org, radi, color, -1)
     new_image = cv.addWeighted(overlay, opacity, image, 1 - opacity, 0.1)
@@ -128,12 +173,18 @@ def trans_circle(image, org, radi, color, opacity):
 
 
 if __name__ == "__main__":
+    list_of_colors = get_list_of_random_rgb_colors_with_decent_contrast(10)
+    list_of_colors.pop(-1)
+    list_of_colors = convert_flat_list_to_list_of_two_values(list_of_colors)
+    fg_color, bg_color = list_of_colors[1]
+    # for color in list_of_colors:
+    #     print(color)
     # creating empty image using np
     image = np.zeros((800, 800, 3), dtype=np.uint8)
     # turn black color into white color image
     image[image == 0] = 255
     # Draw the transparent circle on the image
-    image = trans_circle(image, (400, 400), 80, (0, 255, 255), 0.4)
+    image = trans_circle(image, (400, 400), 80, list_of_colors[2][0], 0.4)
     # create rectangle with corners around that.
     image = rect_corners(image, [255, 255, 290, 290], (0, 0, 0), th=5)
     # Draw text with background
@@ -143,8 +194,8 @@ if __name__ == "__main__":
         (30, 60),
         fonts=cv.FONT_HERSHEY_TRIPLEX,
         scaling=1.11,
-        color=(0, 255, 0),
-        bg_color=(25, 25, 75),
+        color=fg_color,
+        bg_color=bg_color,
     )
     cv.imshow("image", image)
     cv.waitKey(0)
